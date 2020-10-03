277c610e8b0723a7a21641c5b884d1d19d672896
Ronda Cowen <cowenhome5@gmail.com>
2020-10-03 10:01:16 -0500

    working on show method for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,12 @@ class OrdersController < ApplicationController
     def index
         @orders = Order.all
     end
+    def show
+        @customer_orders = @orders.each do |order|
+            customer_order.find_by(order_id: @order.id, customer_id: current_customer.id)
+        end        
+    end
+    def new 
+        @order = order.new(customer_id: [current_customer.id])
+    end 
 end
