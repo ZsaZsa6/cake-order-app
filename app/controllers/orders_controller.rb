f2715c2b27446e5a60c079c324a1839717cc608c
Ronda Cowen <cowenhome5@gmail.com>
2020-10-03 12:06:42 -0500

    working on index and show pages for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,21 @@
 class OrdersController < ApplicationController
     def index
         @orders = Order.all
+              
     end
+
     def show
         @customer_orders = @orders.each do |order|
             @order.find_by(order_id: @order.id, customer_id: current_customer.id)
-        end        
+        end          
     end
+    
     def new 
         @order = order.new(customer_id: [current_customer.id])
-    end 
+    end
+    
+    private
+    def order_params
+        params.permit(:order_id,:description, :created_at, :updated_at, :customer_id, :cake_id)
+      end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create", as: "session"
   delete '/sessions', to: "sessions#destroy", as: "logout"
-  
+  resources :orders
   resources :customers
   resources :cakes
-  resources :orders
+  
 
 end
