9dd5a7ab1663c6bd372720f03d6b1bbbf1590efb
Ronda Cowen <cowenhome5@gmail.com>
2020-10-13 21:15:39 -0500

    still working on show action

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -1,18 +1,24 @@
 class CakesController < ApplicationController
+    before_action :set_cake, only: [:show, :edit, :update, :destroy]
     def new
-        @cake = Cake.new(cake_id: [order.id])
+        @cake = Cake.new(cake_id: [cake.id])
     end
     def create
         Cake.create(cake_params)
     end
     def show
-        @cake = Cake.find(param[:id])
+        @cake = Cake.find_by()
     end
 
     private
+    def set_cake
+    
+    @cake = Cake.find(params[:id])
+
+    end
 
     def cake_params
-      params.permit(:tiers, :flavors, :fillings, :frosting, :size)
+      params.require(:cake).permit(:tiers, :flavors, :fillings, :frosting, :size)
     end  
-   
+    
 end
