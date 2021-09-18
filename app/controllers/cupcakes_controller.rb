350cfd7172f40f0af720234ce9965c9368645661
Ronda Cowen <cowenhome5@gmail.com>
2021-09-18 15:11:20 -0500

    add to products and cupcakes controllers

--- app/controllers/cupcakes_controller.rb
+++ app/controllers/cupcakes_controller.rb
@@ -1,1 +1,9 @@
-
+class CupCakesController < ApplicationController
+    def new 
+        @cupcake = Cupcake.new
+    end
+    def
+        create
+        @cupcake = Cupcake.(cupcake_params)
+    end
+end
