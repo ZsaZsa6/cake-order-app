98d1fcbda000a3ecf8a498956674ccb2710882a6
Ronda Cowen <cowenhome5@gmail.com>
2021-10-02 08:55:23 -0500

    add to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,14 @@
 class ProductsController < ApplicationController
+    def new
+        @product = Product.create
+    end
+
     def show
         @product = Product.find(params[:id])
+    end
+
+    private
+    def product_params
+        params.require(:product).permit(:type)
     end
 end
